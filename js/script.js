aa18ca555a3c90ed2b2da63b3da61e8e09e4c0f5
tubasa <hupa04386021@gmail.com>
2026-05-12 06:37:59 +0900

    お問い合わせフォーム　Jquery実装

--- js/script.js
+++ js/script.js
@@ -26,14 +26,44 @@ const swiper = new Swiper('.swiper', {
 /*アコーディオン（FAQ) */
 
 $(function() {
-  $('.faq__question').on('click', function() {
-    const $item = $(this).closest('.faq__item');
+    $('.faq__question').on('click', function() {
+        const $item = $(this).closest('.faq__item');
 
-    $('.faq__item').not($item)
-      .removeClass('is-open')
-      .find('.faq__answer').slideUp(300);
+        $('.faq__item').not($item)
+            .removeClass('is-open')
+            .find('.faq__answer').slideUp(300);
 
-    $item.toggleClass('is-open');
-    $item.find('.faq__answer').slideToggle(300);
-  });
+            $item.toggleClass('is-open');
+            $item.find('.faq__answer').slideToggle(300);
+    });
+});
+
+/* 送信ボタンの制御 */
+$(document).ready(function() {
+    const $submitBtn = $('#js-submit');
+    const $form = $('#form');
+
+    console.log('$form:', $form); // ← ここに追加
+    console.log('$submitBtn:', $submitBtn); 
+
+    $form.find('input, textarea').on('change', function() {
+        console.log('変更検知');
+
+        const allFilled = 
+            $form.find('input[type="text"]').toArray().every(el => $(el).val() !== '') &&
+            $form.find('input[type="email"]').val() !== '' &&
+            $form.find('input[type="tel"]').val() !== '' &&
+            $form.find('textarea').val() !== '' &&
+            $form.find('input[type="radio"]:checked').length > 0;
+
+        console.log('allFilled:', allFilled); 
+
+        $submitBtn.prop('disabled', !allFilled);
+
+        if (allFilled) {
+            $submitBtn.addClass('is-active');
+        } else {
+            $submitBtn.removeClass('is-active');
+        }
+    });
 });
